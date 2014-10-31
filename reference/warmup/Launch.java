1676b0b9d0ca995f766981f422565ece86d70e52
Erik Steringer <deveriks@live.com>
2014-10-31 08:45:31 -0700

    pushing up example code

---
+++ reference/warmup/Launch.java
@@ -0,0 +1,12 @@
+/*
+ * 
+ *
+ */
+
+public class Launch {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the demo, a new window with the program should launch shortly...");
+		TestDisplay tdisplay = new TestDisplay();
+		tdisplay.start();
+	}
+}
