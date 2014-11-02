9aa0b1d57ac9fb116f1e3fd30934351f8c60ed28
Wesley Gyde <wesley.gyde@gmail.com>
2014-11-02 13:48:40 -0800

    collision interface drafted

---
+++ physics/Point.java
@@ -0,0 +1,10 @@
+public abstract class Point{
+
+	//----------------------
+	//--| Access Methods |--
+	//----------------------
+
+	public abstract double getX();
+	public abstract double getY();
+
+}
