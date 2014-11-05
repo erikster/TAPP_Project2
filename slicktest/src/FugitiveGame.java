d4d1a6b5a06fa4e3d37e16f394ed6dec08e334ec
Erik Steringer <deveriks@live.com>
2014-11-04 23:33:22 -0800

    pushing up more dev stuff

---
+++ slicktest/src/FugitiveGame.java
@@ -0,0 +1,19 @@
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class FugitiveGame extends BasicGame {
+	public FugitiveGame(String gamename) {
+		super(gamename);
+	}
+	
+	@Override
+	public void init(GameContainer gc) throws SlickException { }
+	
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException { }
+	
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException { }
+}
